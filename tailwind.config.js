a58cad854067931831a2516d3b114b86657bea77
M1ngsan636 <suhardiman@airasia.com>
2025-05-28 11:37:34 +0700

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.{html,js}", // Scans all HTML/JS files in your project
-    // Add other file paths if needed (e.g., "./src/**/*.{html,js}")
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // <- Tells Tailwind where to scan for classnames
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
