659d48612b0a3f288a7791ce547cc12a22420512
M1ngsan636 <suhardiman@airasia.com>
2025-05-28 10:10:15 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./**/*.{html,js}", // Scans all HTML/JS files in your project
+    // Add other file paths if needed (e.g., "./src/**/*.{html,js}")
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
