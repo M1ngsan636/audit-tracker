c3c1c2b6b164b6be03efa70743d7d5fe73d2186f
M1ngsan636 <suhardiman@airasia.com>
2025-05-28 14:21:18 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect } from "react";
 
-"use client"; // important since you use useEffect
 
 export default function Home() {
   useEffect(() => {
